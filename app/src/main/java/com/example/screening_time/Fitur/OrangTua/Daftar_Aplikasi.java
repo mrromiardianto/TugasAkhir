c63f1b5a0ce5b4e96269c05bf3ca97c98efaa87d
mrromiardianto <mr.romiardianto@gmail.com>
2021-01-05 23:10:07 +0700

    Y

---
+++ app/src/main/java/com/example/screening_time/Fitur/OrangTua/Daftar_Aplikasi.java
@@ -0,0 +1,14 @@
+package com.example.screening_time.Fitur.OrangTua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Daftar_Aplikasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar__aplikasi);
+    }
+}
